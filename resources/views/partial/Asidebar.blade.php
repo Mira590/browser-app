d4e84ed10bd92a75f3daed1466c95ccdb704df9c
Mirajan Najmezada <m.najmezada@azb.local>
2025-12-16 16:08:19 +0430

    sidebar

--- resources/views/partial/Asidebar.blade.php
+++ resources/views/partial/Asidebar.blade.php
@@ -1,10 +1,10 @@
 <div class="sidebar-wrapper" data-simplebar="true">
 			<div class="sidebar-header">
 				<div>
-					<img src="assets/images/logo-icon.png" class="logo-icon" alt="logo icon">
+					<img src='{{asset("assets/images/logo-icon.png")}}' class="logo-icon" alt="logo icon">
 				</div>
 				<div>
-					<h4 class="logo-text">Rocker</h4>
+					<h4 class="logo-text">IT STOCK</h4>
 				</div>
 				<div class="toggle-icon ms-auto"><i class='bx bx-arrow-back'></i>
 				</div>
@@ -18,53 +18,42 @@
 						<div class="menu-title">Dashboard</div>
 					</a>
 					<ul>
-						<li> <a href="index.html"><i class='bx bx-radio-circle'></i>Default</a>
-						</li>
-						<li> <a href="index2.html"><i class='bx bx-radio-circle'></i>Alternate</a>
-						</li>
-						<li> <a href="index3.html"><i class='bx bx-radio-circle'></i>Graphical</a>
+						
 						</li>
 					</ul>
 				</li>
 				<li>
 					<a href="javascript:;" class="has-arrow">
-						<div class="parent-icon"><i class="bx bx-category"></i>
+						<div class="parent-icon"><i class="bx bx-user"></i>
 						</div>
-						<div class="menu-title">Application</div>
+						<div class="menu-title">User Management</div>
 					</a>
 					<ul>
-						<li> <a href="app-emailbox.html"><i class='bx bx-radio-circle'></i>Email</a>
+						<li> <a href="app-emailbox.html"><i class='bx bx-user-plus'></i>Create User</a>
 						</li>
-						<li> <a href="app-chat-box.html"><i class='bx bx-radio-circle'></i>Chat Box</a>
+						<li> <a href="app-chat-box.html"><i class='bx bx-menu'></i>All Users</a>
 						</li>
-						<li> <a href="app-file-manager.html"><i class='bx bx-radio-circle'></i>File Manager</a>
-						</li>
-						<li> <a href="app-contact-list.html"><i class='bx bx-radio-circle'></i>Contatcs</a>
-						</li>
-						<li> <a href="app-to-do.html"><i class='bx bx-radio-circle'></i>Todo List</a>
-						</li>
-						<li> <a href="app-invoice.html"><i class='bx bx-radio-circle'></i>Invoice</a>
-						</li>
-						<li> <a href="app-fullcalender.html"><i class='bx bx-radio-circle'></i>Calendar</a>
-						</li>
+						
 					</ul>
 				</li>
 				<li class="menu-label">Setting</li>
 				
 				<li>
 					<a href="javascript:;" class="has-arrow">
-						<div class="parent-icon"><i class='bx bx-cart'></i>
+						<div class="parent-icon"><i class='bx bx-folder'></i>
 						</div>
-						<div class="menu-title">eCommerce</div>
+						<div class="menu-title">Asset Management</div>
 					</a>
 					<ul>
-						<li> <a href="ecommerce-products.html"><i class='bx bx-radio-circle'></i>Products</a>
+						<li> <a href="ecommerce-products.html"><i class='bx bx-plus-circle'></i>Add Branch</a>
 						</li>
-						<li> <a href="ecommerce-products-details.html"><i class='bx bx-radio-circle'></i>Product Details</a>
+						<li> <a href="ecommerce-products-details.html"><i class='bx bx-menu'></i>All Branches</a>
 						</li>
-						<li> <a href="ecommerce-add-new-products.html"><i class='bx bx-radio-circle'></i>Add New Products</a>
+						<li> <a href="ecommerce-add-new-products.html"><i class='bx bx-plus-circle'></i> Add Category</a>
 						</li>
-						<li> <a href="ecommerce-orders.html"><i class='bx bx-radio-circle'></i>Orders</a>
+						<li> <a href="ecommerce-orders.html"><i class='bx bx-category'></i>All Categories</a>
+							<li> <a href="ecommerce-orders.html"><i class='bx bx-plus-circle'></i>New Item</a>
+								<li> <a href="ecommerce-orders.html"><i class='bx bx-laptop'></i>All Items</a>
 						</li>
 					</ul>
 				</li>
