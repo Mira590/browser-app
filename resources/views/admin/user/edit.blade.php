31f0d9478d75a538902d351303b46e07a3599ab4
Mirajan Najmezada <m.najmezada@azb.local>
2026-01-18 12:28:24 +0430

    update

--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -25,12 +25,12 @@
 									<div class="card-body">
 										<div class="d-flex flex-column align-items-center text-center">
 				                        <img id="pre"
-                                         src="{{ asset( Auth::user()->photo) }}"
+                                         src="{{ asset( $user->photo) }}"
                                          alt="Admin"
                                          class=""
                                          width="200">
 											<div class="mt-3">
-												<h4>{{Auth::user()->first_name}}</h4>
+												<h4>{{$user->first_name}}</h4>
 												<p class="text-secondary mb-1">{{$user->email}}</p>
 												
 											</div>
